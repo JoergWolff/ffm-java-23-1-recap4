dd6398da404b608554994e816f2532f30e4e3c43
JoergWolff <neuerfisch@wolffclan.de>
2023-10-02 19:59:16 +0200

    create some dummies on backend

--- backend/src/main/java/de/neuefische/backend/todo/TodoRepository.java
+++ backend/src/main/java/de/neuefische/backend/todo/TodoRepository.java
@@ -10,7 +10,17 @@ import java.util.Map;
 @Repository
 class TodoRepository {
 
-    private final Map<String, Todo> todos = new HashMap<>(Map.of("1", new Todo("1", "Test", TodoStatus.OPEN)));
+    /* private final Map<String, Todo> todos = new HashMap<>(Map.of("1", new Todo("1", "Test", TodoStatus.OPEN)));*/
+
+    private final Map<String, Todo> todos = new HashMap<>(Map.of(
+            "1", new Todo("1", "Keller aufräumen", TodoStatus.OPEN),
+            "2", new Todo("2", "Regal schleifen und streichen", TodoStatus.IN_PROGRESS),
+            "3", new Todo("3", "Staubsaugen am Mittwoch", TodoStatus.DONE),
+            "4", new Todo("4", "Geschirr abwaschen", TodoStatus.OPEN),
+            "5", new Todo("5", "Fussleisten befestigen", TodoStatus.IN_PROGRESS),
+            "6", new Todo("6", "Staubsaugen am Mittwoch", TodoStatus.OPEN)
+    ));
+
 
     public List<Todo> getAll() {
         return new ArrayList<>(todos.values());
